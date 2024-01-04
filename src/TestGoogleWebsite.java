89cf7e6c32c49238c78cd2c8143f66569af145f9
Naruapon <naruapon@webmail.npru.ac.th>
2024-01-04 17:01:20 +0700

    updated kru test

--- src/TestGoogleWebsite.java
+++ src/TestGoogleWebsite.java
@@ -34,60 +34,12 @@ class TestGoogleWebsite {
         driver.get("https://www.google.com");
         
         WebElement search_box = driver.findElement(By.name("q"));
-        search_box.sendKeys("NPRU");
+        search_box.sendKeys("KRU");
         search_box.sendKeys(Keys.ENTER);
  
         String result = driver.findElement(By.className("VuuXrf")).getText();
-        assertEquals("มหาวิทยาลัยราชภัฏนครปฐม",result);
+        assertEquals("Kanchanaburi Rajabhat University",result);
         driver.close();     
 	}
-	
-	@Test
-	void testSearchByKeyword3() {
-        WebDriver driver = null;
-        System.setProperty("webdriver.chrome.driver", "D:\\webdriver\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        
-        WebElement search_box = driver.findElement(By.name("q"));
-        search_box.sendKeys("NPRU");
-        search_box.sendKeys(Keys.ENTER);
- 
-        String result = driver.findElement(By.className("VuuXrf")).getText();
-        assertEquals("มหาวิทยาลัยราชภัฏนครปฐม",result);
-        driver.close();     
-	}
-	
-	
-	@Test
-	void testSearchByKeyword4() {
-        WebDriver driver = null;
-        System.setProperty("webdriver.chrome.driver", "D:\\webdriver\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        
-        WebElement search_box = driver.findElement(By.name("q"));
-        search_box.sendKeys("NPRU");
-        search_box.sendKeys(Keys.ENTER);
- 
-        String result = driver.findElement(By.className("VuuXrf")).getText();
-        assertEquals("มหาวิทยาลัยราชภัฏนครปฐม",result);
-        driver.close();     
-	}
-	
-	@Test
-	void testSearchByKeyword5() {
-        WebDriver driver = null;
-        System.setProperty("webdriver.chrome.driver", "D:\\webdriver\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        
-        WebElement search_box = driver.findElement(By.name("q"));
-        search_box.sendKeys("NPRU");
-        search_box.sendKeys(Keys.ENTER);
- 
-        String result = driver.findElement(By.className("VuuXrf")).getText();
-        assertEquals("มหาวิทยาลัยราชภัฏนครปฐม",result);
-        driver.close();     
-	}
+
 }
